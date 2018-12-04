81523b7d23c1a772c998bd03d896455d57a66c29
ino <padosolee89@gmail.com>
2018-12-04 09:12:28 +0900

    -

--- public/javascripts/precium.js
+++ public/javascripts/precium.js
@@ -1,10 +1,22 @@
 
-var Web3 = require("web3");
+var Web3 = require('web3');
+
+
 function initInsatnce(){
+
+    if( typeof web3 != 'undefined'){
+        //web3 = new Web3(web3.currentProvider);
+    }else{
+        //web3 = new Web3("https://ropsten.infura.io/v3/aed4570c599e4438a46a982092eee546");
+    }
+
+
    // web3 = new Web3("https://ropsten.infura.io/v3/aed4570c599e4438a46a982092eee546");
 
-    web3 = new Web3(new Web3.providers.HttpProvider("http://relaxed_lamport:8545"));
+    //web3 = new Web3(new Web3.providers.HttpProvider("http://relaxed_lamport:8545"));
+//Console.log(Web3.version);
+  /*  Web3 = new Web();
     var network_version = web3.version.network;
     console.log(network_version);
-
+*/
 }
