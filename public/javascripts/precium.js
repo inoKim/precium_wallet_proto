91648e15a5062fd22006e36e8218d5c6f4be4d8d
ino <padosolee89@gmail.com>
2018-12-04 18:12:02 +0900

    basic architecture fixed.

--- public/javascripts/precium.js
+++ public/javascripts/precium.js
@@ -1,22 +1,51 @@
-
-var Web3 = require('web3');
+document.write("");
 
 
 function initInsatnce(){
 
-    if( typeof web3 != 'undefined'){
-        //web3 = new Web3(web3.currentProvider);
-    }else{
-        //web3 = new Web3("https://ropsten.infura.io/v3/aed4570c599e4438a46a982092eee546");
-    }
+    console.log('initInstance()');
+}
 
+function setNotice(msg){
+    $("#tv_notice").text(msg);
+}
+function test(){
+    var url_connectnode = 'http://localhost:3000/test';
+    $.ajax({
+            url: url_connectnode,
+            method : 'get' ,
+            success:function(data){
+                setNotice(data);
+            }
+        }
+    )
+}
 
-   // web3 = new Web3("https://ropsten.infura.io/v3/aed4570c599e4438a46a982092eee546");
+function connectNode(){
+    var url_connectnode = 'http://localhost:3000/connectNode';
+    $.ajax({
+            url: url_connectnode,
+            method : 'get' ,
+            success:function(data){
+                setNotice(data);
+            }
+        }
+    )
+}
+function check_balance(){
+    var url_connectnode = 'http://localhost:3000/confirmBalance';
+    $.ajax({
+            url: url_connectnode,
+            method : 'get' ,
+            data : {
+                addr : $("#tv_addr").val()
+            },
+            success:function(data){
+                $("#ll_balance").val(" " + data + " wei");
+            }
+        }
+    )
+}
+function genWallet(){
 
-    //web3 = new Web3(new Web3.providers.HttpProvider("http://relaxed_lamport:8545"));
-//Console.log(Web3.version);
-  /*  Web3 = new Web();
-    var network_version = web3.version.network;
-    console.log(network_version);
-*/
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
 
+
+var gateWay = "https://ropsten.infura.io/v3/aed4570c599e4438a46a982092eee546";
+var default_passwd = "1234567890";
+
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider(gateWay));
+console.log("web3 version : " + web3.version.toString());
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Precium wallet.' });
 });
 
+router.get('/test', function (req, res, next) {
+   console.log('called test function.');
+
+   var Accounts = require('web3-eth-accounts');
+   var accounts = new Accounts(gateWay);
+
+   var obj = web3.eth.accounts.create(default_passwd);
+   console.log("addr : " + obj.address);
+   console.log("priv key : " + obj.privateKey)
+});
+
+router.get('/confirmBalance' , function (req , res , next){
+    var _response = "";
+    var _addr = req.query.addr;
+    web3.eth.getBalance(_addr , (err, data)=>{
+        console.log(err);
+        console.log("balance :" + data + "wei");
+        if( typeof err != 'undefined'){
+           res.send("occurred error");
+        }else{
+            res.send(data);
+        }
+    }
+  )
+});
+
+router.get('/connectNode', function (req , res , next){
+    var result = "";
+    if( typeof web3 == 'undefined'){
+        result = "[fail]";
+    }else{
+        result = "[Success connect to server...]";
+        web3.eth.getBalance(
+           // '0xe399d4d877bbedaa5f1a9e2f4fe8106c45972976'
+            '0xcB89fD5A04DE73e3361996f0F0eC21b9165B1641'
+            ,(err,data)=>{
+            console.log(err);
+            console.log("balance :" + data + "wei");
+            });
+
+    }
+    res.send(result);
+});
 module.exports = router;
