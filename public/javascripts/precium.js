3211cbd038d2579ef53db3b3091aba311581658c
ino <padosolee89@gmail.com>
2018-12-07 11:56:15 +0900

    Implement basic functions for precium wallet

--- public/javascripts/precium.js
+++ public/javascripts/precium.js
@@ -3,7 +3,7 @@ function setNotice(msg){
     $("#tv_notice").text(msg);
 }
 function appendHistory(msg){
-    $("#ta_history").text($("#ta_history").text() +"\n"+ msg);
+    $("#ta_history").text($("#ta_history").text() +"\r\n"+ msg);
 }
 function test(){
     var url_connectnode = 'http://localhost:3000/test';
@@ -12,6 +12,21 @@ function test(){
             method : 'get' ,
             success:function(data){
                 setNotice(data);
+            }
+        }
+    )
+}
+
+function check_balance_ropsten(){
+    var url_connectnode = 'http://localhost:3000/getbalance_ropsten';
+    $.ajax({
+            url: url_connectnode,
+            method : 'get' ,
+            data : {
+                "addr" : $("#tv_addr").val()
+            },
+            success:function(data){
+                appendHistory($("#tv_addr").val() + " : " + data + " wei left on ropsten. ");
             }
         }
     )
@@ -53,12 +68,38 @@ function generateWallet(){
                 passwd: $('#input_passwd').val(),
             },
             success:function(data){
-                $("#ll_balance").text(" " + data + " wei");
-                appendHistory( $("#tv_addr").val() + "'s balance :" + data + "wei");
+
+                let addr = data['address'];
+                let privKey = data['privateKey'];
+
+                let _text = "\r\nadress : " + addr ;
+                _text += "\r\nprivateKey : " + privKey;
+
+                appendHistory(_text);
+                
             }
         }
     )
 }
+
+function transaction_ropsten(){
+    var url_connectnode = 'http://localhost:3000/transaction_ropsten';
+    $.ajax({
+            url: url_connectnode,
+            method : 'post' ,
+            data : {
+                from_addr : $('#tv_from_addr').val(),
+                from_privk: $('#tv_from_privk').val(),
+                to_addr: $('#tv_to_addr').val(),
+                amount : $('#tv_amount').val()
+            },
+            success:function(data){
+                appendHistory("Transaction(Ropsten) hash data : 0x"+data);
+            }
+        }
+    )
+}
+
 function transaction(){
     var url_connectnode = 'http://localhost:3000/transaction';
     $.ajax({
@@ -66,13 +107,12 @@ function transaction(){
             method : 'post' ,
             data : {
                 from_addr : $('#tv_from_addr').val(),
-                from_passwd: $('#tv_from_passwd').val(),
+                from_privk: $('#tv_from_privk').val(),
                 to_addr: $('#tv_to_addr').val(),
                 amount : $('#tv_amount').val()
             },
             success:function(data){
-                $("#ll_balance").text(" " + data + " wei");
-                appendHistory( $("#tv_addr").val() + "'s balance :" + data + "wei");
+                appendHistory("Transaction hash data : 0x"+data);
             }
         }
     )
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,16 @@ var contractAddr = "0x9387A03BbB50e0AF52DFd56Aa977f8Ab3E346135"; //precium_contr
 var contractInterface ;
 var precium_contract ;
 
+const jNetworkType = {
+    null : "invalid network",
+    "AppTestToken" : "ropsten" ,
+    "PreciumToken" : "main-net"
+}
 
+const networkType = ["invalid network" , "ropsten" ,"main-net"];
+const ConfimationText = [null , "AppTestToken" , "PreciumToken"];
+
+var res = null;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,34 +36,45 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/connectNode', function (req , res , next){
-
-    var result = "";
+    let result = "";
+    let _res = res;
     if( typeof web3 == 'undefined'){
         result = "[fail]";
     }else{
         result = "[Connect to server successfully]";
-        //precium_contract = new web3.eth.Contract(abi , contractAddr); //version 1.0
-
-        ///*
-        contractInterface = web3.eth.contract(abi); // version 0.20
-        precium_contract = contractInterface.at(contractAddr);
-        const res = precium_contract.name(); // version 0.20.x
-        //*/
-        //////  * Have to change function what can confirm contract's version or what.*
-
-        //var ret = precium_contract.methods.name(); // version 1.0
-        if( res == "AppTestToken" ){
-            result += "\r\nLoading smart-contract(Ropsten) done."
-        }else if( res == "PreciumToken"){
-            result += "\r\nLoading smart-contract(mainNet) done."
-        }else{
-            result += "\r\nFail to load smart-contract.";
-        }
-        //////
+        
+        precium_contract = new web3.eth.Contract(abi , contractAddr); //version 1.0
+        res = precium_contract.methods.name().call((error , echo )=> {
+            
+            let nType = null;
+            for( key in jNetworkType){
+                if( echo == key ){
+                    nType = jNetworkType[key];
+                }
+            }
+            
+            console.log("network Type : " + nType);
+            
+            result += nType == null ? "\r\nFail to connect smart-contract" : "\r\nSuccess to connect smart-contract("+nType+")";
+            _res.send(result);
+        });
     }
-    res.send(result);
+    
 });
 
+
+router.get('/getbalance_ropsten' ,function(req, res , next ){
+    if( web3 == null ){
+        res.send("Initialize first.");
+    }
+
+    let _res = res;
+    let addr = req.query.addr;
+    web3.eth.getBalance(addr,(error , echo) => {
+        console.log(echo);
+        res.send(echo);
+    });
+});
 
 router.get('/test', function (req, res, next) {
     console.log('called test function.');
@@ -78,49 +98,132 @@ router.post('/createAccount', function (req, res, next) {
     });
 });
 
-
 router.get('/confirmBalance' , function (req , res , next){
+    let _res = res ;
     var _addr = req.query.addr;
-    var ret = precium_contract.balanceOf(_addr);  // return big-integer.
-    res.send(ret.toString());
+    res = precium_contract.methods.balanceOf(_addr).call((error , echo) => {
+        console.log("Left balance : " + echo);
+        _res.send(echo.toString());
+    });
+    
+    
 });
 
-router.post('/transaction' , function (req, res , next){
+async function getNonce( _address){
+
+    let nonce_tmp ;
+    await web3.eth.getTransactionCount(_address).then((echo ) => {
+        nonce_tmp = echo;
+    });
+    return nonce_tmp;
+}
+router.post('/transaction_ropsten' , function (req, res , next){
     var _from_addr = req.body.from_addr;
-    var _from_passwd = req.body.from_passwd;
+    var _from_privk = req.body.from_privk;
     var _to_addr = req.body.to_addr;
     var _amount = req.body.amount;
 
-    
+    var _res = res;
 
-    /*var bRet = precium_contract.transferFrom(
-        _from_addr,
-        _to_addr,
-        _amount,
-        );
-
-    */
-
+    var _nonce = getNonce(_from_addr);
+     
     try{
-
         var Tx = require('ethereumjs-tx');
         var rawTx = {
             to : _to_addr,
             from : _from_addr ,
-            value : _amount
+            value : _amount,
+            nonce: '0x00',
+            //nonce: _nonce,
+            //gasPrice: '0x09184e72a000',
+            gasPrice: '0xc350',
+            gasLimit: '0xc350',
         };
 
         var tx = new Tx(rawTx);
-        const privateKey = Buffer.from(_from_passwd , 'hex');
+        const privateKey = Buffer.from(_from_privk , 'hex');
         tx.sign(privateKey);
 
         var serializedTx = tx.serialize();
-        var tansactionHash = web3.sendRawTransaction('0x'+ serializedTx.toString('hex'));
+        var tansactionHash = web3.eth.sendSignedTransaction(
+            '0x'+ serializedTx.toString('hex') ,(echo) => {
+                console.log("data echo : " + echo);
+                _res.send(echo);
+            });
+            
     }catch (e) {
         console.error(e);
     }
 
-    console.log(txHash);
+    //console.log( "transaction hash : "+ tansactionHash);
 });
+
+
+router.post('/transaction' , function (req, res , next){
+    var _from_addr = req.body.from_addr;
+    var _from_privk = req.body.from_privk;
+    var _to_addr = req.body.to_addr;
+    var _amount = req.body.amount;
+    var _res = res;
+
+    let transferData = precium_contract.methods.transfer(_to_addr , Number(_amount)).encodeABI();
+
+    try{
+        var Tx = require('ethereumjs-tx');
+        var rawTx = {
+            to : _to_addr,
+            from : _from_addr ,
+            value : _amount,
+            nonce: '0x00',
+            //nonce: _nonce,
+            //gasPrice: '0x09184e72a000',
+            data : transferData,
+            gasPrice: '0xc350',
+            gasLimit: '0xc350',
+        };
+
+        var tx = new Tx(rawTx);
+        const privateKey = Buffer.from(_from_privk , 'hex');
+        tx.sign(privateKey);
+
+        var serializedTx = tx.serialize();
+
+        var tansactionHash = web3.eth.sendSignedTransaction(
+            '0x'+ serializedTx.toString('hex') ,(echo) => {
+                console.log("data echo : " + echo);
+                _res.send(echo);
+            });
+            
+    }catch (e) {
+        console.error(e);
+    }
+});
+
+
 module.exports = router;
 
+
+
+function oldLogic (){
+
+        /* // connect to server and contract.
+        contractInterface = web3.eth.contract(abi); // version 0.20
+        precium_contract = contractInterface.at(contractAddr);
+        const res = precium_contract.name(); // version 0.20.x
+        
+        * Have to change function what can confirm contract's version or what.*
+        if( res == "AppTestToken" ){
+            result += "\r\nLoading smart-contract(Ropsten) done."
+        }else if( res == "PreciumToken"){
+            result += "\r\nLoading smart-contract(mainNet) done."
+        }else{
+            result += "\r\nFail to load smart-contract.";
+        }
+        */
+
+
+        // Balance confirmation.
+        //var ret = precium_contract.balanceOf(_addr);  // return big-integer. (version 0.20.x)
+
+
+}
